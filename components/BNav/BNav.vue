9e5a9f14e0d97115e06c04ddeb72ad8023f1e9ab
ltfei <ltfei@ltfei.cn>
2023-08-25 19:21:22 +0800

    feat: 登录图标

--- components/BNav/BNav.vue
+++ components/BNav/BNav.vue
@@ -6,10 +6,15 @@ defineOptions({
 
 <template>
   <nav class="b-nav">
-    <div class="logo">
-      <img src="https://www.ltfei.cn/img/logo.d05ba22d.webp" alt="logo" />
+    <div class="left-entry">
+      <div class="logo">
+        <img src="https://www.ltfei.cn/img/logo.d05ba22d.webp" alt="logo" />
+      </div>
+      <div class="title">Ltfei Blog</div>
     </div>
-    <div class="title">Ltfei Blog</div>
+    <div class="right-entry">
+      <nav-user />
+    </div>
   </nav>
 </template>
 
@@ -17,25 +22,32 @@ defineOptions({
 nav.b-nav {
   height: 60px;
   background: @bg-color;
-  display: flex;
-  align-items: center;
   box-shadow: 0px 2px 10px 0px @black-opacity-1;
   box-sizing: border-box;
   padding: 0 10px;
-  .logo {
-    width: 50px;
-    height: 50px;
-    border-radius: 50%;
-    overflow: hidden;
-    img {
-      width: 100%;
-      height: 100%;
-    }
+  display: flex;
+  justify-content: space-between;
+  .left-entry,
+  .right-entry {
+    display: flex;
+    align-items: center;
   }
-  .title {
-    font-size: 20px;
-    font-weight: 500;
-    color: @text-color;
+  .left-entry {
+    .logo {
+      width: 50px;
+      height: 50px;
+      border-radius: 50%;
+      overflow: hidden;
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+    .title {
+      font-size: 20px;
+      font-weight: 500;
+      color: @text-color;
+    }
   }
 }
 </style>
---
+++ components/NavUser/NavUser.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { User as IconUser } from '@icon-park/vue-next'
+defineOptions({
+  name: 'NavUser'
+})
+
+const isLogin = false
+</script>
+
+<template>
+  <div class="nav-user">
+    <nuxt-link v-if="!isLogin" to="/login" class="to-login-icon">
+      <icon-user></icon-user>
+    </nuxt-link>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.to-login-icon {
+  width: 20px;
+  height: 20px;
+  color: @text-color;
+  border: 1px solid @text-color;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
