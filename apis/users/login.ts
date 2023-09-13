1f6bac60704984a58a470f98b02522c2547c98d6
ltfei <ltfei@ltfei.cn>
2023-09-13 22:34:54 +0800

    feat(apis): 完善类型

--- apis/users/login.ts
+++ apis/users/login.ts
@@ -2,17 +2,22 @@ import axiosRequest, { type Response } from '~/utils/request'
 
 export const init = (): Response<{
   uuid: string
+  loginMethods: string[]
 }> => {
   return axiosRequest({
     url: '/users/login/init'
   })
 }
 
-export const qqConnectLogin = (uuid: string) => {
+export const qqConnectLogin = (
+  uuid: string
+): Response<{
+  qqLoginUrl: string
+}> => {
   return axiosRequest({
     url: '/users/login/qqConnect',
     method: 'post',
-    params: {
+    data: {
       uuid
     }
   })
