f6f3ae418e0d65e3071abec73a2efe1dae589159
ltfei <ltfei@ltfei.cn>
2023-09-16 21:40:00 +0800

    chore: element

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import { lessAdditionalData } from './utils/theme'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@vueuse/nuxt'],
+  modules: ['@vueuse/nuxt', '@element-plus/nuxt'],
   css: ['@/assets/main.less'],
   vite: {
     css: {
