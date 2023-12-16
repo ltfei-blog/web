7403ed413c352b15e2ddddd0f3833728f00f8876
ltfei <ltfei@ltfei.cn>
2023-12-17 01:12:14 +0800

    feat(pages/index): 首页sidebar

---
+++ components/IndexSidebar/IndexSidebar.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'IndexSidebar'
+})
+</script>
+
+<template>
+  <div class="miniprogram component"></div>
+  <div class="footer component">
+    <!-- todo: 动态获取 -->
+    <div class="copyright">&copy;2021-2024 litfa all rights reserved.</div>
+    <a href="https://beian.miit.gov.cn/" target="_blank">豫ICP备2021024510号-3</a>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.component {
+  background-color: @bg-color;
+  margin: 15px 0;
+}
+.miniprogram {
+  height: 120px;
+}
+.footer {
+  background-color: unset;
+  div,
+  a {
+    color: @text-color-secondary;
+    font-size: 14px;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -11,24 +11,50 @@ const data = await useAsyncData('getArticles', () => listApi())
 
 <template>
   <!-- card demo -->
-  <b-card
-    v-for="i in data.data.value?.data"
-    :key="i.id"
-    :title="i.title"
-    :to="`/p/${i.id}`"
-    router
-    :auto-collapse="470"
-    :desc="i.desc"
-    :username="i.author_data.username"
-    :avatar="i.author_data.avatar"
-    :cover="i.cover"
-    :date="i.create_time"
-  ></b-card>
+  <div class="index">
+    <div class="cards">
+      <b-card
+        v-for="i in data.data.value?.data"
+        :key="i.id"
+        :title="i.title"
+        :to="`/p/${i.id}`"
+        router
+        :auto-collapse="470"
+        :desc="i.desc"
+        :username="i.author_data.username"
+        :avatar="i.author_data.avatar"
+        :cover="i.cover"
+        :date="i.create_time"
+      ></b-card>
+    </div>
+    <div class="sidebar">
+      <index-sidebar />
+    </div>
+  </div>
 </template>
 
 <style lang="less" scoped>
+.index {
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  .cards {
+    width: 100%;
+    max-width: 800px;
+    margin: 0 8px;
+  }
+
+  .sidebar {
+    margin: 0 8px;
+    width: 300px;
+    flex-shrink: 0;
+    .min-width(700px, {
+      display: none;
+    });
+  }
+}
 .b-card {
-  margin: 15px;
+  margin: 15px 0;
   display: block;
   box-shadow: 3px 3px 5px 0 @black-opacity-1;
   transition: all 0.3s;
