59f7b0316311aae9ea80c4f5db56ffc042d6d36c
ltfei <ltfei@ltfei.cn>
2024-03-15 18:36:13 +0800

    feat(components/LoginCard): 轮询登录状态

--- apis/users/login.ts
+++ apis/users/login.ts
@@ -38,3 +38,44 @@ export const qqConnectLogin = (
     }
   })
 }
+
+export const loginStatus = {
+  /**
+   * 未登录
+   */
+  notLogin: 0,
+  /**
+   * 已扫码
+   */
+  scanCode: 1,
+  /**
+   * 已获取qq互联登录url 已创建登录地址
+   */
+  getQqConnectUrl: 2,
+  /**
+   * 登录成功
+   */
+  loginSucceed: 10,
+  /**
+   * 登录失败(用户取消登录)
+   */
+  loginFailedUserCancel: 20,
+  /**
+   * 登录失败(其他原因)
+   */
+  loginFailedOtherCause: 21,
+  /**
+   * 登录失败(登录超时) (不在数据库体现，仅返回数据时判断时间)
+   */
+  loginFailedTimeout: 22
+}
+
+export const getStatus = (uuid: string): Response<{ status: number }> => {
+  return axiosRequest({
+    url: '/users/login/getStatus',
+    method: 'post',
+    data: {
+      uuid
+    }
+  })
+}
--- components/LoginCard/LoginCard.vue
+++ components/LoginCard/LoginCard.vue
@@ -1,6 +1,17 @@
 <script setup lang="ts">
-import { init as initApi, getQqConnectUrl as qqConnectLoginApi } from '~/apis/users/login'
+import {
+  init as initApi,
+  getQqConnectUrl as qqConnectLoginApi,
+  getStatus as getStatusApi,
+  loginStatus
+} from '~/apis/users/login'
 import { getQrCode } from '~/apis/users/wxLogin'
+import {
+  CheckOne as IconCheckOne,
+  EmotionUnhappy as IconEmotionUnhappy,
+  Attention as IconAttention
+} from '@icon-park/vue-next'
+
 defineOptions({
   name: 'LoginCard'
 })
@@ -22,32 +33,111 @@ const qqConnectLogin = async () => {
 // 获取微信小程序码
 const getWxQrcode = async () => {
   const res = await getQrCode(uuid!)
-  qrCode.value = URL.createObjectURL(new Blob([res]))
+  const url = URL.createObjectURL(new Blob([res]))
+  nextTick(() => {
+    qrCode.value = url
+    console.log(qrCode.value)
+  })
+
   loading.value = false
 }
 
-if (initData.data?.value?.data.loginMethods.includes('wx_miniprogram')) {
+if (process.client && initData.data?.value?.data.loginMethods.includes('wx_miniprogram')) {
   await getWxQrcode()
 }
-loading.value = false
 
-// todo: 轮询登录状态
+const loginStatusView = ref<'failed' | 'tips' | 'success' | null>(null)
+const message = ref('')
+const getStatus = async () => {
+  const res = await getStatusApi(uuid!)
+  const status = res.data.status
+
+  const callbacks = {
+    [loginStatus.notLogin]: () => {
+      loginStatusView.value = null
+      message.value = ''
+    },
+    [loginStatus.scanCode]: () => {
+      loginStatusView.value = 'tips'
+      message.value = '扫码成功，请在手机确认登录'
+    },
+    [loginStatus.getQqConnectUrl]: () => {
+      loginStatusView.value = 'tips'
+      message.value = '请在新页面继续完成登录'
+    },
+    [loginStatus.loginSucceed]: () => {
+      loginStatusView.value = 'success'
+      message.value = '登录成功'
+      clearInterval(interval)
+    },
+    [loginStatus.loginFailedUserCancel]: () => {
+      loginStatusView.value = 'failed'
+      message.value = '登录失败，已取消登录'
+      clearInterval(interval)
+    },
+    /**
+     * 登录超时
+     * todo: 超时直接刷新二维码
+     */
+    [loginStatus.loginFailedTimeout]: () => {
+      loginStatusView.value = 'failed'
+      message.value = '登录超时'
+      clearInterval(interval)
+    },
+    [loginStatus.loginFailedOtherCause]: () => {
+      loginStatusView.value = 'failed'
+      message.value = '登录失败'
+      clearInterval(interval)
+    }
+  }
+
+  callbacks[status]()
+}
+let interval: NodeJS.Timer
+if (process.client) {
+  interval = setInterval(() => {
+    getStatus()
+  }, 2000)
+
+  onUnmounted(() => {
+    clearInterval(interval)
+  })
+}
 </script>
 
 <template>
-  <!-- <a-spin :spinning="loading"> -->
-  <div class="login-card">
-    <div class="qrcode">
-      <h3>微信扫码登录</h3>
-      <img :src="qrCode" alt="" />
-    </div>
-    <div class="other">
-      <h3>其他方式登录</h3>
-      <div class="other-method qq" @click="qqConnectLogin">qq登录</div>
-    </div>
-    <div class="tips"></div>
-  </div>
-  <!-- </a-spin> -->
+  <client-only>
+    <a-spin :spinning="loading">
+      <div class="login-card">
+        <div class="qrcode">
+          <h3>微信扫码登录</h3>
+          <img :src="qrCode" alt="" v-if="loginStatusView == null" />
+          <div class="status login-failed" v-if="loginStatusView == 'failed'">
+            <icon-emotion-unhappy
+              theme="outline"
+              size="90"
+              fill="var(--success)"
+              :strokeWidth="6"
+            />
+            {{ message }}
+          </div>
+          <div class="status login-tips" v-if="loginStatusView == 'tips'">
+            <icon-attention theme="outline" size="90" fill="var(--success)" :strokeWidth="6" />
+            {{ message }}
+          </div>
+          <div class="status login-success" v-if="loginStatusView == 'success'">
+            <icon-check-one theme="outline" size="90" fill="var(--success)" :strokeWidth="6" />
+            {{ message }}
+          </div>
+        </div>
+        <div class="other">
+          <h3>其他方式登录</h3>
+          <div class="other-method qq" @click="qqConnectLogin">qq登录</div>
+        </div>
+        <div class="tips"></div>
+      </div>
+    </a-spin>
+  </client-only>
 </template>
 
 <style lang="less" scoped>
@@ -69,6 +159,12 @@ loading.value = false
     img {
       width: 80%;
       margin-top: 10px;
+    }
+    .status {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      margin: 25px 0;
     }
   }
   .other {
