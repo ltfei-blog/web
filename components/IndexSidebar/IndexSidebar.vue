beaa3fdc9437c5c176ac0e5aa6652f5ee43fb426
ltfei <ltfei@ltfei.cn>
2024-03-21 09:10:16 +0800

    feat(components/IndexSidebar): 小程序

--- components/IndexSidebar/IndexSidebar.vue
+++ components/IndexSidebar/IndexSidebar.vue
@@ -5,7 +5,13 @@ defineOptions({
 </script>
 
 <template>
-  <div class="miniprogram component"></div>
+  <div class="miniprogram component">
+    <img class="image" src="~/assets/gh_e9be314c6626_258.jpg" alt="" />
+    <div class="text">
+      <div class="title">打开小程序</div>
+      <div class="subTitle">手机阅读更方便</div>
+    </div>
+  </div>
   <div class="footer component">
     <!-- todo: 动态获取 -->
     <div class="copyright">&copy;2021-2024 litfa all rights reserved.</div>
@@ -20,6 +26,24 @@ defineOptions({
 }
 .miniprogram {
   height: 120px;
+  display: flex;
+  align-items: center;
+  .image {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    margin: 0 20px;
+  }
+  .text {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    .subTitle {
+      color: @text-color-regular;
+      margin-top: 8px;
+    }
+  }
 }
 .footer {
   background-color: unset;
